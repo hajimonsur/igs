d83b56610526eb2bc545fa37c0079c4c3b4e5823
hajimonsur <taiwomonsuru29@gmail.com>
2026-05-09 20:10:52 +0100

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-
 function Footer() {
   return (
     <footer className="bg-dark text-white text-center py-3">
       <div className="container">
-        <p className="mb-0">&copy; 2025 Iponri Grammar School. All rights reserved.</p>
+        <p className="mb-0">
+          &copy; 2026 Iponri Grammar School. All rights reserved.
+        </p>
+        <p className="mt-2">
+          Developed by{" "}
+          <a
+            href="https://hajimonsur.github.io/taiwo-monsuru/index.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            XhinaTech
+          </a>
+        </p>
       </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
